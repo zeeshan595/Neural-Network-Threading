141eb5abf39654ba61907106afaa190928fddf00
Zeeshan Abid <zeeshan595@googlemail.com>
2017-08-27 20:31:36 +0100

    Iris Data Set Test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 NN::Base* CreateNetwork()
 {
-    return new NN::MFNN({21, 10, 4}, {
+    return new NN::MFNN({4, 7, 3}, {
         Activation::ActivationType::NONE,   //First must be none due to input layer
         Activation::ActivationType::LOGISTIC_SIGMOID,
         Activation::ActivationType::LOGISTIC_SIGMOID
@@ -16,16 +16,16 @@ void TrainingCallBack(double progress, double msr, std::vector<double> weights)
 
 int main()
 {
-    NN::Normalize(CarEvaluationData::dataset, { 0, 1, 2, 3 });
-    NN::Randomize(CarEvaluationData::dataset);
+    NN::Normalize(IrisData::dataset, { 0, 1, 2, 3 });
+    NN::Randomize(IrisData::dataset);
 
     //Actual Neural Network
     NN::MFNN* my_network = (NN::MFNN*)CreateNetwork();
 
     std::cout << "PSO Threading Test" << std::endl;
-    std::vector<double> good_weights = NN::PSO(CarEvaluationData::dataset, 20, 1000, &CreateNetwork, &TrainingCallBack);
+    std::vector<double> good_weights = NN::PSO(IrisData::dataset, 20, 1000, &CreateNetwork, &TrainingCallBack);
     my_network->SetWeights(good_weights);
-    std::cout << "MSR: " << my_network->GetMeanSquaredError(CarEvaluationData::dataset) << std::endl;
+    std::cout << "MSR: " << my_network->GetMeanSquaredError(IrisData::dataset) << std::endl;
 
     std::cout << "End Of Program" << std::endl;
     std::cin.get();
